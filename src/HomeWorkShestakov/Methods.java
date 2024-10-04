9804935aebe22bcd5cd2bc70c97a55c869a2bdd4
Мариночка МУР <dzonsond330@gmail.com>
2024-10-04 17:38:33 +0300

    commit:Lesson23-24

--- src/HomeWorkShestakov/Methods.java
+++ src/HomeWorkShestakov/Methods.java
@@ -17,17 +17,36 @@ public class Methods {
         printFullName(name, surname);
         String[] array = {"Вася", "привет", "как", "жизнь молодая?" };
         System.out.println(Arrays.toString(cornerValuesArray(array)));
+        int[] intArray = {1, 3, 5, 2, 6, -3};
+        System.out.println(Arrays.toString(cornerValuesArray(intArray)));
+        char[] charArray = {'a','f', 'A', 't'};
+        System.out.println(Arrays.toString(cornerValuesArray(charArray)));
     }
 
+    /**
+     * Возвращает квадрат переданного целого чмсла
+     * @param a целое число
+     * @return возвращает квадрат переданного целого числа
+     */
     public static int square(int a) {
         return a * a;
     }
 
+    /**
+     * Принимает две переменных типа String, имя и фамилию, и выводыит в консоль полное имя
+     * @param name переменная String имя
+     * @param surname переменная String фамилия
+     */
     public static void printFullName(String name, String surname) {
         System.out.println("Full name: " + name + " " + surname);
     }
 
-    public static String[] cornerValuesArray(String[] array) {
+    /**
+     * Находит из переменных типа String самую маленькую и самую большую строку.
+     * @param array varargs с переменными типа String.
+     * @return возвращает массив с наименьшей и наибольшей строками.
+     */
+    public static String[] cornerValuesArray(String... array) {
         int min = array[0].length();
         int max = array[0].length();
         String sMin = "";
@@ -43,5 +62,47 @@ public class Methods {
             }
         }
         return new String[]{sMin, sMax};
+    }
+
+    /**
+     * Находит из переменных типа int самый маленький и самый большой элемент.
+     * @param array varargs с переменными типа int.
+     * @return возвращает массив с наименьшим и наибольшим элементами.
+     */
+    public static int[] cornerValuesArray(int... array) {
+        int min = array[0];
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return new int[]{min, max};
+    }
+
+    /**
+     * Находит из переменных типа char самый маленький и самый большой элемент
+     * @param array varargs с переменными типа char.
+     * @return возвращает массив с наименьшим и наибольшим элементами.
+     */
+    public static char[] cornerValuesArray(char... array) {
+        int min = array[0];
+        int max = array[0];
+        char charMin = array[0];
+        char charMax = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+                charMin = array[i];
+            }
+            if (max < array[i]) {
+                max = array[i];
+                charMax = array[i];
+            }
+        }
+        return new char[]{charMin, charMax};
     }
 }
