d06b7fc6c2cb15cd6d607644696ea7858d028a67
Мариночка МУР <dzonsond330@gmail.com>
2024-10-11 15:30:16 +0300

    commit:Lesson25-26.

--- src/HomeWorkShestakov/Methods.java
+++ src/HomeWorkShestakov/Methods.java
@@ -1,62 +1,52 @@
 package HomeWorkShestakov;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Methods {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введите ваше целое число.");
-        int square = scan.nextInt();
-        System.out.println("Квадрат вашего целого числа равен " + square(square));
-        System.out.println("Пожалуйста введите ваше имя.");
-        String name = scan.nextLine();
-        name = scan.nextLine();
-        System.out.println("Введите пожалуйста вашу фамилию.");
-        String surname = scan.nextLine();
-        printFullName(name, surname);
-        String[] array = {"Вася", "привет", "как", "жизнь молодая?" };
-        System.out.println(Arrays.toString(cornerValuesArray(array)));
-        int[] intArray = {1, 3, 5, 2, 6, -3};
-        System.out.println(Arrays.toString(cornerValuesArray(intArray)));
-        char[] charArray = {'a','f', 'A', 't'};
-        System.out.println(Arrays.toString(cornerValuesArray(charArray)));
-    }
-
-    /**
-     * Возвращает квадрат переданного целого чмсла
-     * @param a целое число
-     * @return возвращает квадрат переданного целого числа
-     */
-    public static int square(int a) {
-        return a * a;
-    }
-
-    /**
-     * Принимает две переменных типа String, имя и фамилию, и выводыит в консоль полное имя
-     * @param name переменная String имя
-     * @param surname переменная String фамилия
-     */
-    public static void printFullName(String name, String surname) {
-        System.out.println("Full name: " + name + " " + surname);
+        String[] string1 = {};
+        System.out.println(Arrays.toString(cornerValuesArray(string1)));
+        String[] string2 = {"AAA", "AAA", "AAA"};
+        System.out.println(Arrays.toString(cornerValuesArray(string2)));
+        String[] string3 = {"Hello", "world", "!"};
+        System.out.println(Arrays.toString(cornerValuesArray(string3)));
+        int[] int1 = {};
+        System.out.println(Arrays.toString(cornerValuesArray(int1)));
+        int[] int2 = {6, 6, 6, 6, 6};
+        System.out.println(Arrays.toString(cornerValuesArray(int2)));
+        int[] int3 = {6, 1, 0, 6, -7};
+        System.out.println(Arrays.toString(cornerValuesArray(int3)));
+        char[] char1 = {};
+        System.out.println(Arrays.toString(cornerValuesArray(char1)));
+        char[] char2 = {'o', 'o', 'o', 'o'};
+        System.out.println(Arrays.toString(cornerValuesArray(char2)));
+        char[] char3 = {'0', 'i', '9', 'y'};
+        System.out.println(Arrays.toString(cornerValuesArray(char3)));
+        System.out.println(factorial(-3));
+        System.out.println(factorial(5));
+        System.out.println(factorial(0));
+        System.out.println(factorial(1));
     }
 
     /**
      * Находит из переменных типа String самую маленькую и самую большую строку.
+     *
      * @param array varargs с переменными типа String.
      * @return возвращает массив с наименьшей и наибольшей строками.
      */
     public static String[] cornerValuesArray(String... array) {
+        if (array.length == 0) {
+            return new String[]{null, null};
+        }
         int min = array[0].length();
         int max = array[0].length();
-        String sMin = "";
-        String sMax = "";
+        String sMin = array[0];
+        String sMax = array[0];
         for (int i = 1; i < array.length; i++) {
             if (min > array[i].length()) {
                 min = array[i].length();
                 sMin = array[i];
-            }
-            if (max < array[i].length()) {
+            } else if (max < array[i].length()) {
                 max = array[i].length();
                 sMax = array[i];
             }
@@ -66,10 +56,14 @@ public class Methods {
 
     /**
      * Находит из переменных типа int самый маленький и самый большой элемент.
+     *
      * @param array varargs с переменными типа int.
      * @return возвращает массив с наименьшим и наибольшим элементами.
      */
     public static int[] cornerValuesArray(int... array) {
+        if (array.length == 0) {
+            return new int[]{0, 0};
+        }
         int min = array[0];
         int max = array[0];
         for (int i = 1; i < array.length; i++) {
@@ -85,10 +79,14 @@ public class Methods {
 
     /**
      * Находит из переменных типа char самый маленький и самый большой элемент
+     *
      * @param array varargs с переменными типа char.
      * @return возвращает массив с наименьшим и наибольшим элементами.
      */
     public static char[] cornerValuesArray(char... array) {
+        if (array.length == 0) {
+            return new char[]{'0', '0'};
+        }
         int min = array[0];
         int max = array[0];
         char charMin = array[0];
@@ -104,5 +102,23 @@ public class Methods {
             }
         }
         return new char[]{charMin, charMax};
+    }
+
+    /**
+     * Находит факториал заданного числа
+     * @param n любое целое число, факториал которого мы ищем
+     * @return возвращает факториал целого числа
+     */
+    public static int factorial(int n) {
+        int result = 1;
+        if (n == 1 || n == 0) {
+            return result;
+        }
+        if (n < 0) {
+            result = n * factorial(n + 1);
+            return result;
+        }
+        result = n * factorial(n - 1);
+        return result;
     }
 }
